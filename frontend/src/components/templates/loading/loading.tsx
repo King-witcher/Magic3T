f431157c1ae28c6c0df27b5f7c5a79f6b30c9cfb
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-02-20 20:01:39 -0300

    add some panel to loading screen

--- frontend/src/components/templates/loading/loading.tsx
+++ frontend/src/components/templates/loading/loading.tsx
@@ -1,9 +1,11 @@
-import { Spinner } from '@/components/atoms'
+import { Panel, Spinner } from '@/components/atoms'
 
 export function Loading() {
   return (
     <div className="flex items-center justify-center h-full w-full">
-      <Spinner className="size-[70px]" />
+      <Panel>
+        <Spinner className="size-[70px]" />
+      </Panel>
     </div>
   )
 }
