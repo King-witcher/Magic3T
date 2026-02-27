98a0e0dac58793b7800ef1d479ad0b0da6160e7c
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-02-27 00:50:47 -0300

    update exception filter to log suspicious 404 requests

--- backend/src/common/filters/http-exception.filter.ts
+++ backend/src/common/filters/http-exception.filter.ts
@@ -1,11 +1,11 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from '@nestjs/common'
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger } from '@nestjs/common'
+import * as Sentry from '@sentry/node'
+import { Request, Response } from 'express'
 import { Socket } from 'socket.io'
-
-/**
- *
- */
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private logger: Logger = new Logger(HttpExceptionFilter.name)
+
   catch(error: HttpException, argumentsHost: ArgumentsHost) {
     const context = argumentsHost.getType()
 
@@ -17,8 +17,17 @@ export class HttpExceptionFilter implements ExceptionFilter {
       }
       case 'http': {
         const ctx = argumentsHost.switchToHttp()
-        const response = ctx.getResponse()
-        response.status(error.getStatus()).json(error.getResponse())
+        const status = error.getStatus()
+        const response = ctx.getResponse<Response>()
+        response.status(status).json(response)
+
+        if (status === 404) {
+          const request = ctx.getRequest<Request>()
+          const message = `Suspicious request from IP ${request.header('True-Client-IP')}: ${request.method} ${request.url}`
+          this.logger.warn(message)
+          Sentry.logger.warn(message)
+        }
+
         break
       }
       case 'rpc': {
