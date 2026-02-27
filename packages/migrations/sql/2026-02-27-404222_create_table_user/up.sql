06b899ceadf9abd65c1443762ee9166b0857bfa3
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-02-27 15:56:26 -0300

    create migration

---
+++ packages/migrations/sql/2026-02-27-404222_create_table_user/up.sql
@@ -0,0 +1,50 @@
+-- Migration generated on 2026-02-27 11:13:42
+
+CREATE TYPE user_role AS ENUM (
+    'player',
+    'admin',
+    'superuser'
+    );
+
+CREATE TYPE user_apex_flag AS ENUM (
+    'challenger',
+    'grandmaster'
+    );
+
+CREATE TABLE "user"
+(
+    id                    INTEGER GENERATED ALWAYS AS IDENTITY,
+    uuid                  uuid UNIQUE        NOT NULL DEFAULT gen_random_uuid(),
+    fb_id                 CHAR(28) UNIQUE,
+    role                  user_role          NOT NULL DEFAULT 'player',
+
+    profile_nickname      VARCHAR(16)        NOT NULL,
+    profile_nickname_slug VARCHAR(16) UNIQUE NOT NULL,
+    profile_nickname_date TIMESTAMP          NOT NULL,
+    profile_icon          SMALLINT           NOT NULL DEFAULT 29,
+
+    rating_score          REAL               NOT NULL,
+    rating_k_factor       REAL               NOT NULL CHECK ( rating_k_factor > 0 ),
+    rating_apex           user_apex_flag,
+    rating_series_played  SMALLINT           NOT NULL DEFAULT 0,
+    rating_date           DATE               NOT NULL DEFAULT NOW(),
+
+    stats_victories       INTEGER            NOT NULL DEFAULT 0 CHECK ( stats_victories >= 0 ),
+    stats_draws           INTEGER            NOT NULL DEFAULT 0 CHECK ( stats_victories >= 0 ),
+    stats_defeats         INTEGER            NOT NULL DEFAULT 0 CHECK ( stats_victories >= 0 ),
+
+    FOREIGN KEY (profile_icon) REFERENCES icon (id) ON DELETE SET DEFAULT
+);
+
+COMMENT ON COLUMN "user".id IS 'User''s internal id';
+COMMENT ON COLUMN "user".uuid IS 'User''s external id';
+COMMENT ON COLUMN "user".fb_id IS 'Firebase Authentication ID';
+COMMENT ON COLUMN "user".rating_k_factor IS 'Dynamic K-Factor used in Elo rating calculation';
+COMMENT ON COLUMN "user".rating_series_played IS 'Number of games played in the initial rating series';
+COMMENT ON COLUMN "user".rating_apex IS 'A flag indicating if the user has reached Challenger or Grandmaster rank';
+
+CREATE INDEX ON "user" (id);
+CREATE INDEX ON "user" (uuid);
+CREATE INDEX ON "user" (fb_id) WHERE fb_id IS NOT NULL;
+CREATE INDEX ON "user" (profile_nickname_slug);
+CREATE INDEX ON "user" (rating_score DESC, rating_date ASC);
