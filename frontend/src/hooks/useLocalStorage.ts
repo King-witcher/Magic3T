447cf6204dc1ca417fed17091398148fee1e0dd1
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-03-08 22:19:38 -0300

    fix local storage

--- frontend/src/hooks/useLocalStorage.ts
+++ frontend/src/hooks/useLocalStorage.ts
@@ -1,34 +1,19 @@
-import { captureException } from '@sentry/react'
 import { useEffect, useState } from 'react'
-import * as z from 'zod'
-import { Console } from '@/lib/console'
 
-export function useLocalStorage<T>(
+export function useLocalStorage<T extends string = string>(
   key: string,
-  initialValue: T,
-  schema?: z.ZodType<T>
+  initialValue: T
 ): [value: T, setValue: (value: T) => void] {
   const [value, setValue] = useState<T>(() => {
     const data = localStorage.getItem(key)
-    if (data) {
-      try {
-        const parsed = JSON.parse(data)
-        return schema ? schema.parse(parsed) : parsed
-      } catch (e) {
-        Console.log(`Error parsing localStorage key "${key}"`)
-        console.error(e)
-        captureException(e)
-        localStorage.setItem(key, JSON.stringify(initialValue))
-        return initialValue
-      }
-    } else return initialValue
+    return (data ?? initialValue) as T
   })
 
   useEffect(() => {
-    if (value === undefined) {
+    if (value === null) {
       localStorage.removeItem(key)
     } else {
-      localStorage.setItem(key, JSON.stringify(value))
+      localStorage.setItem(key, value)
     }
   }, [value, key])
 
