1e2cce8c82c9e256e4328adad867a1c661d81298
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-03-28 13:19:01 -0300

    fix rating update calculation

--- apps/backend/src/modules/match/client-sync.service.ts
+++ apps/backend/src/modules/match/client-sync.service.ts
@@ -28,25 +28,25 @@ export class ClientSyncService {
 
     const newOrderRank = summary.rankConverter.getRankFromElo(
       summary.order.newRating.elo,
-      summary.order.row.rating_ranked_count + 1,
-      summary.order.row.rating_apex_flag
+      summary.order.newRating.rankedCount,
+      summary.order.newRating.apexFlag
     )
     const newChaosRank = summary.rankConverter.getRankFromElo(
       summary.chaos.newRating.elo,
-      summary.chaos.row.rating_ranked_count + 1,
-      summary.chaos.row.rating_apex_flag
+      summary.chaos.newRating.rankedCount,
+      summary.chaos.newRating.apexFlag
     )
 
     // Calculate LP gains
-    const isRankedAfterGame = (rankedCount: number) =>
-      summary.ranked && !!ratingConfig && rankedCount + 1 >= ratingConfig.min_ranked_count
+    const hasLpGain = (rankedCount: number) =>
+      summary.ranked && rankedCount >= ratingConfig.min_ranked_count
 
-    const orderLpGain = isRankedAfterGame(summary.order.row.rating_ranked_count)
+    const orderLpGain = hasLpGain(summary.order.row.rating_ranked_count)
       ? summary.rankConverter.getTotalLP(summary.order.newRating.elo) -
         summary.rankConverter.getTotalLP(summary.order.row.rating_score)
       : null
 
-    const chaosLpGain = isRankedAfterGame(summary.chaos.row.rating_ranked_count)
+    const chaosLpGain = hasLpGain(summary.chaos.row.rating_ranked_count)
       ? summary.rankConverter.getTotalLP(summary.chaos.newRating.elo) -
         summary.rankConverter.getTotalLP(summary.chaos.row.rating_score)
       : null
